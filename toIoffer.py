2dce2d50de5ffc1c584e47bab8339ffcb5b7cdd3
lilili001 <2861166132@qq.com>
2018-06-08 14:52:19 +0800

    add encrypt

--- toIoffer.py
+++ toIoffer.py
@@ -1,5 +1,14 @@
+#coding:utf-8
+
 import csv
+import hashlib
 import json
+import random
+import re
+import string
+import time
+
+from random import Random
 
 from googletrans import Translator
 
@@ -8,8 +17,19 @@ from ali1688.helper import str_replace_new
 import os
 
 import sys
+from random import Random
+from hashlib import md5
 
 root_path = os.path.abspath('.')
+
+
+def fnmd5(str):
+    m = hashlib.md5()
+    m.update(str.encode("utf8"))
+    return m.hexdigest()
+
+def salt():
+    return ''.join(random.sample(string.ascii_letters + string.digits, 8))
 
 def start():
 
@@ -17,15 +37,27 @@ def start():
 
     lines = []
     #r = csv.reader(open(new_img_path+'data_hyg_trans.csv' ,'r'  ,encoding='gbk' ))
-    r = csv.reader(open(new_img_path+'data_baita_01_trans.csv' ,'r'   ))
+    r = csv.reader(open(new_img_path+'data_baita_01_trans_01.csv' ,'r'  ))
     #r = csv.reader(open('F:\laragon\www\python\image\data.csv','r'))
+
 
     for index,row in enumerate(r):
 
         title = row[8].replace(u'\0xb4',u' ')
-
+        title = title.replace(u'\0x81',u' ')
+        #title.encode('utf-8').decode('gbk','ignore').encode('utf-8','ignore')
+        #title.decode('gbk').encode('utf-8')
         #if index >  0 and  len(row) > 0 and row is not None  :
         print(index)
+
+        reg = r'offer/(.*?).html'
+        pattern = re.compile(reg)
+        out = re.findall(pattern, row[7])
+
+        filename = time.strftime("%Y%m%d-%H%M%S", time.localtime()) + '-' +out[0]
+        str = salt() + '-' + filename
+        #print(fnmd5( str  ))
+        #print( fnmd5('hellsdfsdfo') )
 
         temp_line = []
         temp_line.append('1010292')
@@ -41,7 +73,7 @@ def start():
         temp_line.append('https://res.cloudinary.com/dzf6pxzir/image/upload/%s/800/2.jpg' % row[2])
         temp_line.append('https://res.cloudinary.com/dzf6pxzir/image/upload/%s/800/3.jpg' % row[2])
         temp_line.append('')
-        temp_line.append('Color:'+row[9] +';Size:'+row[5])
+        temp_line.append('Color:'+row[9] +';Size:'+row[5]+';' + 'sku:'+ row[3]  )
         temp_line.append('Woman Dress')
         temp_line.append('Summer Dress')
         temp_line.append('Big size dress')
@@ -69,7 +101,7 @@ def start():
 
 
     #new_csv = open('F:\laragon\www\python\image\data_trans.csv','w',newline='')
-    new_csv = open(new_img_path+'/baita_ioffer.csv', 'a', newline=''  )
+    new_csv = open(new_img_path+'/baita_ioffer.csv', 'a', newline=''   )
     writer = csv.writer(new_csv)
     writer.writerow(( 'category' ,'title','product_type','brand','condition',
                       'description','quantity','price','shipping','image1','image2','image3',
@@ -81,3 +113,7 @@ def start():
     print(lines)
 
 start()
+
+# /usr/bin/python
+# -*- coding:utf-8 -*-
+
